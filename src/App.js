11a9ad5635913d9854b1a9019d15943f46807c96
Raghuprasaad Iyer <raghupviyer@gmail.com>
2021-01-06 18:01:03 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React, {useState, useEffect} from 'react';
+import './App.css'
+import $1 from './images/1$.png'
+import $5 from './images/5$.png'
+import $10 from './images/10$.png'
+import $20 from './images/20$.png'
+import $100 from './images/100$.png'
+import $500 from './images/500$.png'
+import $2000 from './images/2000$.png'
 
 function App() {
   const [billAmount, setbillAmount] = useState('')
@@ -58,21 +66,19 @@ function App() {
   }
   
   return (
-    <div className="">
-      <center>
-        <h1>Cash Register Manager App</h1>
+    <div className="App">
+        <h1 className="Navbar">Cash Register Manager App</h1>
         <input type="number" placeholder="Bill Amount" value={billAmount} onChange={event => setbillAmount(event.target.value)}/>
         {billState && <input type="number" placeholder="Cash You Gave" value={cashAmount} onChange={event => setcashAmount(event.target.value)}/>}
         {cashState && <button onClick={calcReturnAmount}>Calculate Return Change</button>}
         <h2>${returnAmount}</h2>
-        <p>$1 x{notes[1]}</p>
-        <p>$5 x{notes[5]}</p>
-        <p>$10 x{notes[10]}</p>
-        <p>$20 x{notes[20]}</p>
-        <p>$100 x{notes[100]}</p>
-        <p>$500 x{notes[500]}</p>
-        <p>$2000 x{notes[2000]}</p>
-      </center>
+        <p><img src={$1} width='130.0px' height='50px'/> <h2>x{notes[1]}</h2></p>
+        <p><img src={$5} width='130.0px' height='50px'/> <h2>x{notes[5]}</h2></p>
+        <p><img src={$10} width='130.0px' height='50px'/> <h2>x{notes[10]}</h2></p>
+        <p><img src={$20} width='130.0px' height='50px'/> <h2>x{notes[20]}</h2></p>
+        <p><img src={$100} width='130.0px' height='50px'/> <h2>x{notes[100]}</h2></p>
+        <p><img src={$500} width='130.0px' height='50px'/> <h2>x{notes[500]}</h2></p>
+        <p><img src={$2000} width='130.0px' height='50px'/> <h2>x{notes[2000]}</h2></p>
     </div>
   );
 }
